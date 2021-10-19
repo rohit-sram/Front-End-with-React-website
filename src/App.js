2816e81581162c01b2701129f271f78feb0f153c
rohit-sram <rohitsram10@outlook.com>
2021-10-19 18:17:53 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="secondary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
+import Main from './components/MainComponent';
+
+import './App.css';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+const store = ConfigureStore();
+
+class App extends Component{
+
+
+  render(){
+
+    return(
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>      
+        </BrowserRouter>
+      </Provider>
     );
+
   }
+
 }
 
 export default App;
